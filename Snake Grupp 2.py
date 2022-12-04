7933c637b0239d0f5923abae24f0ce8a6618f7bd
S-Gert <gert.sillaots@eek.ee>
2022-12-04 02:15:09 +0200

    test commit

--- Snake Grupp 2.py
+++ Snake Grupp 2.py
@@ -2,11 +2,12 @@ import turtle as tl
 import time
 import random
 
+# Test push
 delaySeconds = 0.1 #time.sleep() funktsiooni muutuja
 
 #mänguaken
 wn = tl.Screen()
-wn.title("snek")
+wn.title("Sneik")
 wn.setup(width = 600, height = 600)
 wn.bgcolor("#000000")
 wn.tracer(0)
@@ -19,6 +20,13 @@ head.color("#FFFFFF")
 head.penup() # ei tõmba joont turtle objektile järgi selle funktsiooniga
 head.goto(0, 0)
 head.direction = "stop"
+
+#toidu objekt
+food = tl.Turtle()
+food.shape("square")
+food.color("#FF0000")
+food.penup()
+food.goto(random.randrange(-280, 280, pixel), random.randrange(-280, 280, pixel))
 
 #game loop
 while True:
